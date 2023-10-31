561ad39e2a4388d87fee1f595570a0667efe9d5b
NY64 <me@ny64.kr>
2023-10-31 14:46:21 +0900

    edit url

--- frontend/lib.c
+++ frontend/lib.c
@@ -2,45 +2,45 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
-#define URL "http://localhost:8080"
+#define URL "https://dpkg.ny64.kr"
 
-int downloadFile(const char *url, const char *output_filename) {
-    CURL *curl;
-    CURLcode res;
+int downloadFile(const char* url, const char* output_filename) {
+  CURL* curl;
+  CURLcode res;
 
-    FILE *output_file = fopen(output_filename, "wb");
-    if (output_file == NULL) {
-        fprintf(stderr, "Failed to open output file for writing\n");
-        return 1;
+  FILE* output_file = fopen(output_filename, "wb");
+  if (output_file == NULL) {
+    fprintf(stderr, "Failed to open output file for writing\n");
+    return 1;
+  }
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, output_file);
+
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
     }
 
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, output_file);
+    curl_easy_cleanup(curl);
+    fclose(output_file);
+  }
 
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        }
-
-        curl_easy_cleanup(curl);
-        fclose(output_file);
-    }
-
-    return 0;
+  return 0;
 }
 
 int pkg_install(char* package) {
-  char* url = (char*)malloc(sizeof(package)+sizeof(URL)+6);
-  char* output = (char*)malloc(sizeof(package)+4);
+  char* url = (char*)malloc(sizeof(package) + sizeof(URL) + 6);
+  char* output = (char*)malloc(sizeof(package) + 4);
   sprintf(url, "%s/%s.dpm", URL, package);
   sprintf(output, "%s.dpm", package);
-  
+
   if (downloadFile(url, output) == 1) {
     return 1;
   };
-  
+
 }
 
 
